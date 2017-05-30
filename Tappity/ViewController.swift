8a47cae8157d8be2d2345eaec8a02e1234ec1858
Hilal Alsameraaii <hilalalsameraaii@b00pler.local>
2017-05-30 13:46:24 +1000

    Basic Gameplay + Colour Changes

--- Tappity/ViewController.swift
+++ Tappity/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var time: UILabel!
+    @IBOutlet var score: UILabel!
+    
+    var gameRunning = false
+    var timeValue = 30
+    var scoreValue = 0
+    
+    var colors = [UIColor.red, UIColor.black, UIColor.yellow, UIColor.orange, UIColor.green, UIColor(red: 0, green: 61/255, blue: 161/255, alpha: 1), UIColor.gray, UIColor.purple, UIColor.magenta, UIColor.blue]
+    
+    @IBAction func buttonPress(sender: UIButton) {
+        if (gameRunning == false) {
+            gameRunning = true
+            scoreValue = 0
+            timeValue = 30
+            countDown()
+        }
+        
+        scoreValue += 1
+        score.text = String(scoreValue)
+        self.view.backgroundColor = colors[Int(arc4random_uniform(UInt32(colors.count)))]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func countDown() {
+        timeValue -= 1
+        time.text = String(timeValue)
+        
+        if (timeValue != 0) {
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { (timer) in
+                self.countDown()
+            }
+        } else {
+            gameRunning = false
+            
+            let gameOver = UIAlertController(title: "Game Over", message: "Your score was \(scoreValue)", preferredStyle: UIAlertControllerStyle.actionSheet)
+            gameOver.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(gameOver, animated: true)
+        }
     }
-
-
+    
 }
 
